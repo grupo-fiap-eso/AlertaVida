1f29952c00507ea940e8109bdb273f8339b10b4d
Lu-baptist <luanabatista20081@gmail.com>
2025-06-05 10:31:06 -0300

    feat(auth): adiciona tela de login e redirecionamento por perfil

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,20 @@
+// src/services/firebaseConfig.js
+
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDYXo3CePsw8A8cIgpMhwdSuxHeZzLGd0M',
+  authDomain: 'alertavida-c7efb.firebaseapp.com',
+  projectId: 'alertavida-c7efb',
+  storageBucket: 'alertavida-c7efb.appspot.com',
+  messagingSenderId: '72223675305',
+  appId: '1:72223675305:web:1c7f2eeff5fe347757eab8',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export {auth, db};
